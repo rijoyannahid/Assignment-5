a402eaa0d3740f6647334aa967d810cb95ac1717
rijoyannahid <nh346304@gmail.com>
2025-08-28 18:01:49 +0600

    Just some necessary edition

---
+++ script.js
@@ -0,0 +1,11 @@
+// --- State ---
+let likes = 0;
+let coins = 100;
+let copies = 2;
+
+// --- Elements ---
+const likeCountEl = document.getElementById('likeCount');
+const coinCountEl = document.getElementById('coinCount');
+const copyCountEl = document.getElementById('copyCount');
+const historyList = document.getElementById('historyList');
+const clearBtn = document.getElementById('clearHistory');
